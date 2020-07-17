380fab2ccda1cdfdb825c984c3b6f5eb8be37257
Martynas Prokopas <promaty@gmail.com>
2020-07-17 02:08:03 +0200

    added postgres to test setup

--- .github/scripts/test/run.sh
+++ .github/scripts/test/run.sh
@@ -5,5 +5,6 @@ cp .env.sample $DIR/.env
 cd $DIR
 export SERVER_IMAGE="$1"
 docker-compose up -d
+docker-compose exec -T server /bin/sh -c "npx wait-on http://localhost:\$SERVER_METRICS_PORT --timeout 20000"
 docker-compose exec -T server yarn test:server
 docker-compose down
