98a49ca3ac9c61012e62ff44cde3d65af31dbd1e
Martynas Prokopas <promaty@gmail.com>
2020-07-29 18:34:35 +0200

    implemented delete endpoint

--- packages/server/src/middlewares/routes.ts
+++ packages/server/src/middlewares/routes.ts
@@ -7,8 +7,7 @@ const router = new Router<DefaultState, DefaultContext>()
 
 router.get('/', (ctx) => ctx.body = { up: true })
 router.post('/files', FilesController.upload, FilesController.create)
-// delete endpoint to do later
-//router.post('/files/:cid\\:delete', FilesController.delete)
+router.post('/files/:cid\\:delete', FilesController.delete)
 router.get('/files', FilesController.findAll)
 router.get('/files/:cid', FilesController.findOne)
 
--- packages/server/test/integration/file-creation.test.ts
+++ packages/server/test/integration/file-creation.test.ts
@@ -1,11 +1,14 @@
 import HttpStatus from 'http-status-codes'
 import request from 'supertest'
 import { Server } from 'http'
+import { Wallet } from 'ethers'
 
 import metricsReporter from '../../src/helpers/metrics-reporter'
 import { FileMeta, ipfs, etherscan } from '@aragonone/ipfs-background-service-shared'
 
-const TEST_OWNER_ADDR = '0x7410937813608C0C9f968C17A44A2bAA336C89c2'
+const TEST_OWNER_ADDR = '0x4e1326002e313a0245FC0a1C2fd8E0684C9a0C5A'
+const TEST_PRIVATE_KEY = '0x407eeeb61674af71138c7013be2a995b0e3323093dce7a4f3e58cf5112db236f'
+const wallet = new Wallet(TEST_PRIVATE_KEY)
 const TEST_FILE_NAME = 'test.txt'
 const TEST_FILE_CONTENT = Buffer.from('testcontent')
 const TEST_FILE_CID = 'QmX4bqZRY5p1sjNDMARQqcEho5SUX2CssoZYs6e3UijEEQ'
@@ -47,18 +50,9 @@ describe('File creation', () => {
       .attach('file', TEST_FILE_CONTENT, TEST_FILE_NAME)
     expect(res.status).toEqual(HttpStatus.OK)
     expect(res.body).toEqual(TEST_FILE_META)
+    expect(await FileMeta.exists({cid: TEST_FILE_CID})).toEqual(true)
+    expect(await ipfs.exists(TEST_FILE_CID)).toEqual(true)
   })
-  
-  // delete endpoint to do later
-  // test('should delete a file', async () => {
-  //   const res = await request(serverURL).post(`/files/${TEST_FILE_CID}:delete`).send({
-  //     signature: 'test'
-  //   })
-  //   expect(res.status).toEqual(HttpStatus.OK)
-  //   expect(res.body).toEqual({
-  //     deleted: true
-  //   })
-  // })
   
   test('should get single file', async () => {
     const res = await request(server).get(`/files/${TEST_FILE_CID}`).send()
@@ -73,5 +67,32 @@ describe('File creation', () => {
       total: 1,
       results: [TEST_FILE_META]
     })
+  })
+  
+  test('should fail to delete a file with bad signature', async () => {
+    const timestamp = Date.now()
+    const signature = 'test'
+    const res = await request(server).post(`/files/${TEST_FILE_CID}:delete`).send({
+      signature,
+      timestamp
+    })
+    expect(res.status).toEqual(HttpStatus.BAD_REQUEST)
+    expect(await FileMeta.exists({cid: TEST_FILE_CID})).toEqual(true)
+    expect(await ipfs.exists(TEST_FILE_CID)).toEqual(true)
+  })
+  
+  test('should delete a file with correct signature', async () => {
+    const timestamp = Date.now()
+    const signature = await wallet.signMessage(timestamp.toString())
+    const res = await request(server).post(`/files/${TEST_FILE_CID}:delete`).send({
+      signature,
+      timestamp
+    })
+    expect(res.status).toEqual(HttpStatus.OK)
+    expect(res.body).toEqual({
+      deleted: true
+    })
+    expect(await FileMeta.exists({cid: TEST_FILE_CID})).toEqual(false)
+    expect(await ipfs.exists(TEST_FILE_CID)).toEqual(false)
   })
 })
