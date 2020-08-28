8aea36e1f12a0d80093ca9768d12b0ee8d56813d
Martynas Prokopas <promaty@gmail.com>
2020-08-28 20:31:50 +0200

    run npm publish inside built docker image

--- .github/scripts/npm-publish.sh
+++ .github/scripts/npm-publish.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-export NPM_TOKEN="$1"
-cd packages/client
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
-npm publish --access public
+DOCKER_IMAGE="$1"
+NPM_TOKEN="$2"
+docker run $DOCKER_IMAGE /bin/sh -c "cd packages/client \
+  && echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc \
+  && npm publish --access public"
